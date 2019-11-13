84e9188d1ec3600713283b9509577baa74e95685
Mathieu Capo <mathieu.capo@iut-rodez.fr>
2019-11-13 09:20:38 +0100

    fix #2 Récupération et affichage des utilisateurs avec énoncé non préparé

--- all_users.php
+++ all_users.php
@@ -11,6 +11,7 @@
 		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 		PDO::ATTR_EMULATE_PREPARES   => false,
 	];
+	
 	try {
 		$pdo = new PDO($dsn, $user, $pass, $options);
 	} catch (PDOException $e) {
